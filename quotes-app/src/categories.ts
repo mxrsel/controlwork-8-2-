d6950969ddce96d244b1f55c6199d4fb3d77d70a
mxrsel <marsel14082006@gmail.com>
2024-10-24 13:17:21 +0600

    bring out array of categories to new file

---
+++ quotes-app/src/categories.ts
@@ -0,0 +1,9 @@
+import {CategoriesProps} from "./types.ts";
+
+export const CATEGORIES: CategoriesProps[] = [
+    {title: 'Star Wars', id: 'star-wars'},
+    {title: 'Famous people', id: 'famous-people'},
+    {title: 'Saying', id: 'saying'},
+    {title: 'Humour', id: 'humour'},
+    {title: 'Motivational', id: 'motivational'},
+]
