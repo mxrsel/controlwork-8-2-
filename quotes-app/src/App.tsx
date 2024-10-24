476635ef7ec4d7b84f0811c2043c6a1e60afee54
mxrsel <marsel14082006@gmail.com>
2024-10-24 15:47:21 +0600

    added page for editing

--- quotes-app/src/App.tsx
+++ quotes-app/src/App.tsx
@@ -1,6 +1,8 @@
 import NavBar from "./containers/NavBar/NavBar";
 import {Route, Routes} from "react-router-dom";
 import QuoteForm from "./containers/QuoteForm/QuoteForm.tsx";
+import Quotes from "./containers/Quotes/Quotes.tsx";
+import EditQuote from "./containers/EditQuote/EditQuote.tsx";
 
 const App = () => {
 return(
@@ -12,8 +14,11 @@ return(
         <div className='main-container'>
             <Routes>
                 <Route path="/" element={<h1>Quote</h1>} />
+                <Route path="/quotes" element={<Quotes />} />
                 <Route path="/new-quote" element={<QuoteForm />} />
-                <Route path="*" element={<h1>Not Found</h1>} />
+                <Route path='/quotes/:categoryId' element={<Quotes />}/>
+                <Route path='/quotes/edit-quotes/:id' element={<EditQuote />}/>
+                <Route path="/*" element={<h1>Not Found</h1>} />
             </Routes>
         </div>
     </>
